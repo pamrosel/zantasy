5427751e75c42755f179f1a1fd60ba9325a67454
pamrosel <pamrosel.is@gmail.com>
2023-06-05 18:05:29 +1000

    removed a tags

--- pages/777antasy.jsx
+++ pages/777antasy.jsx
@@ -1,4 +1,5 @@
 import React, { Suspense } from 'react'
+import Head from 'next/head'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import CloseX from '../components/CloseX'
@@ -9,10 +10,19 @@ const Spline = React.lazy(() => import('@splinetool/react-spline'));
 export default function Listen() {
 
   return (
+  <>
     <motion.div exit={{opacity:0}}>
       <div className={styles.container}>
+        <Head>
+          <link rel="preconnect" href="https://www.youtube.com/" crossOrigin/>
+          <link rel="dns-prefetch" href="https://www.youtube.com/" />
+          <link rel="preconnect" href="https://prod.spline.design" crossOrigin/>
+          <link rel="dns-prefetch" href="https://prod.spline.design" />
+        </Head>
+
         <HalfLoader />
         <CloseX/>
+        
         <motion.div 
           initial={{ opacity: 0}}
           animate={{ opacity: 1}}
@@ -36,9 +46,7 @@ export default function Listen() {
             </motion.div>
 
             <motion.div className='mt-[25px] mb-[100px] md:mb-[15vh] p-[15px] md:p-0'>              
-              {/* <h4 className="text-[#4a4dff] pb-2">
-                {`We recorded this thing in the hot summer of 2021 in a little shed in Stafford across from the tavern. Dubsy recorded it standing in front of a white pedestal fan on the highest setting. It's our sweaty baby and first recording, picked up by Gimmie who released it as their first on Gimmie Records.`}
-              </h4> */}
+
               
               <h3 className="text-[#4a4dff]">
 
@@ -46,16 +54,16 @@ export default function Listen() {
                 <span className="font-['UnifrakturMaguntia']">recorded &amp; mixed by </span> Luke Walsh<br/><br/>
 
                 <span className="font-['UnifrakturMaguntia']">GET</span> 12&quot; VINYL RECORD @<br/>
-                <span className="font-['UnifrakturMaguntia']">label</span> <Link href="https://gimmiezine.bandcamp.com/album/777antasy" target="_blank" rel="noreferrer"><a className='text-[#4a4dff] border-[#4a4dff]'>Gimmie Records</a></Link><br/>
-                <span className="font-['UnifrakturMaguntia']">bris</span> <Link href="https://www.sonicsherpa.com.au/store/guppy-777antasy-VY14481" target="_blank" rel="noreferrer"><a className='text-[#4a4dff] border-[#4a4dff]'>Sonic Sherpa</a></Link><br/>
-                <span className="font-['UnifrakturMaguntia']">bris</span> <Link href="https://rockinghorse.net/index.php?route=product/product&path=1_27&product_id=95361&sort=pd.name&order=DESC&limit=50" target="_blank" rel="noreferrer"><a className='text-[#4a4dff] border-[#4a4dff]'>Rocking Horse</a></Link><br/>
-                <span className="font-['UnifrakturMaguntia']">syd</span> <Link href="https://repressedrecords.com/products/guppy-777antasy-lp" target="_blank" rel="noreferrer"><a className='text-[#4a4dff] border-[#4a4dff]'>Repressed Records</a></Link><br/>
-                <span className="font-['UnifrakturMaguntia']">melb</span> <Link href="https://lulusmelb.com/products/guppy-777antasy-lp" target="_blank" rel="noreferrer"><a className='text-[#4a4dff] border-[#4a4dff]'>{`Lulu's`}</a></Link><br/>
+                <span className="font-['UnifrakturMaguntia']">label</span> <Link className='text-[#4a4dff] border-[#4a4dff]' href="https://gimmiezine.bandcamp.com/album/777antasy" target="_blank" rel="noreferrer">Gimmie Records</Link><br/>
+                <span className="font-['UnifrakturMaguntia']">bris</span> <Link className='text-[#4a4dff] border-[#4a4dff]' href="https://www.sonicsherpa.com.au/store/guppy-777antasy-VY14481" target="_blank" rel="noreferrer">Sonic Sherpa</Link><br/>
+                <span className="font-['UnifrakturMaguntia']">bris</span> <Link className='text-[#4a4dff] border-[#4a4dff]' href="https://rockinghorse.net/index.php?route=product/product&path=1_27&product_id=95361&sort=pd.name&order=DESC&limit=50" target="_blank" rel="noreferrer">Rocking Horse</Link><br/>
+                <span className="font-['UnifrakturMaguntia']">syd</span> <Link className='text-[#4a4dff] border-[#4a4dff]' href="https://repressedrecords.com/products/guppy-777antasy-lp" target="_blank" rel="noreferrer">Repressed Records</Link><br/>
+                <span className="font-['UnifrakturMaguntia']">melb</span> <Link className='text-[#4a4dff] border-[#4a4dff]' href="https://lulusmelb.com/products/guppy-777antasy-lp" target="_blank" rel="noreferrer">{`Lulu's`}</Link><br/>
                 <br/>
 
                 <span className="font-['UnifrakturMaguntia']">LISTEN</span>/STREAM IT<br/>
-                <span className="font-['UnifrakturMaguntia']">on</span> <Link href="https://open.spotify.com/album/38WkdpbGUVcC8UfPkF77LX" target="_blank" rel="noreferrer"><a className='text-[#4a4dff] border-[#4a4dff]'>Spotify</a></Link><br/>
-                <span className="font-['UnifrakturMaguntia']">on</span> <Link href="https://music.apple.com/us/album/777antasy/1687306976" target="_blank" rel="noreferrer"><a className='text-[#4a4dff] border-[#4a4dff]'>Apple Music</a></Link><br/><br/>
+                <span className="font-['UnifrakturMaguntia']">on</span> <Link className='text-[#4a4dff] border-[#4a4dff]' href="https://open.spotify.com/album/38WkdpbGUVcC8UfPkF77LX" target="_blank" rel="noreferrer">Spotify</Link><br/>
+                <span className="font-['UnifrakturMaguntia']">on</span> <Link className='text-[#4a4dff] border-[#4a4dff]' href="https://music.apple.com/us/album/777antasy/1687306976" target="_blank" rel="noreferrer">Apple Music</Link><br/><br/>
 
                 <span className="font-['UnifrakturMaguntia']">SONGS</span><br/>
                 <span className="font-['UnifrakturMaguntia']">1</span> Creepin&apos;<br/>
@@ -63,10 +71,10 @@ export default function Listen() {
                 <span className="font-['UnifrakturMaguntia']">3</span> Crystal Hog<br/>
                 <span className="font-['UnifrakturMaguntia']">4</span> Karaoke Song<br/>
                 <span className="font-['UnifrakturMaguntia']">5</span> Thunder Under the Mud&nbsp;
-                <Link href="/thunderunderthemud"><a className='text-[#4a4dff] border-[#4a4dff]'>clip</a></Link>
+                <Link className='text-[#4a4dff] border-[#4a4dff]' href="/thunderunderthemud">clip</Link>
                 <br/>
                 <span className="font-['UnifrakturMaguntia']">6</span> Lipshitz&nbsp;
-                <Link href="/lipshitz"><a className='text-[#4a4dff] border-[#4a4dff]'>clip</a></Link><br/>
+                <Link className='text-[#4a4dff] border-[#4a4dff]' href="/lipshitz">clip</Link><br/>
                 <span className="font-['UnifrakturMaguntia']">7</span> Spirit Song
               </h3>
             </motion.div>
@@ -74,6 +82,7 @@ export default function Listen() {
         </main>
       </div>
     </motion.div>
+    </>
   )
 }
 
