4580db24d59a164a20d07b4538becb9c5e80442b
pamrosel <pamrosel.is@gmail.com>
2023-06-05 16:36:18 +1000

    updated head

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,18 @@
 import '../styles/globals.css'
 import { AnimatePresence } from 'framer-motion'
+import Head from 'next/head'
 
 function MyApp({ Component, pageProps, router } = this.props) {
   
   return(
     <AnimatePresence mode='wait'>
+          <Head>
+          <title>GUPPY</title>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+          <meta name="description" content="GUPPY - HAILING FROM BRISBANE, MEANJIN. Enter Cool 3d World." />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="manifest" href="/site.webmanifest"/>
+        </Head>
       <Component {...pageProps} key={router.route}/>
     </AnimatePresence>
   )
