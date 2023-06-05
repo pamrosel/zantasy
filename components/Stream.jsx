55c86a2920d169f6c17f2c70a1d84fe8b129c972
pamrosel <pamrosel.is@gmail.com>
2023-06-05 16:21:58 +1000

    deleted stream

--- components/Stream.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Stream = () => {
-  return (
-    <iframe className="border-0 w-full h-[373px]" src="https://bandcamp.com/EmbeddedPlayer/album=237102607/size=large/bgcol=333333/linkcol=FFFFFF/artwork=none/transparent=true/" seamless><a href="https://gimmiezine.bandcamp.com/album/777antasy">777antasy by Guppy</a></iframe>
-  )
-}
-
-export default Stream
