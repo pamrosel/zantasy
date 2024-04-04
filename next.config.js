071187fcf50190fe0b2723e53c4a9d45f6d3e907
pamrosel <pamrosel.is@gmail.com>
2024-04-04 23:05:09 +1000

    added google analytics 4

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+const withOptimizedImages = require("next-optimized-images");
+
+module.exports = withOptimizedImages({
+  googleAnalytics: {
+    id: "G-T6HY1842QY", // Replace with your Measurement ID
+  },
+});
