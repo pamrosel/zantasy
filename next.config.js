a32c2c4f76aed8f1fa00be16f643c9868e8b85d4
pamrosel <pamrosel.is@gmail.com>
2024-04-04 23:43:42 +1000

    tracking GA4 with nextjs-google-analytics plugin

--- next.config.js
+++ next.config.js
@@ -5,11 +5,3 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
-
-const withOptimizedImages = require("next-optimized-images");
-
-module.exports = withOptimizedImages({
-  googleAnalytics: {
-    id: "G-T6HY1842QY", // Replace with your Measurement ID
-  },
-});
--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,30 @@
-import '../styles/globals.css'
-import { AnimatePresence } from 'framer-motion'
-import { Analytics } from '@vercel/analytics/react';
-import Head from 'next/head'
+import "../styles/globals.css";
+import { AnimatePresence } from "framer-motion";
+import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps, router } = this.props) {
-  
-  return(
-    <AnimatePresence mode='wait'>
-          <Head>
-          <title>GUPPY</title>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
-          <meta name="description" content="GUPPY - HAILING FROM BRISBANE, MEANJIN. Enter Cool 3d World." />
-          <link rel="icon" href="/favicon.ico" />
-          <link rel="manifest" href="/site.webmanifest"/>
-        </Head>
-      <Component {...pageProps} key={router.route}/>
+  return (
+    <AnimatePresence mode="wait">
+      <Head>
+        <title>GUPPY</title>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <meta
+          name="description"
+          content="GUPPY - HAILING FROM BRISBANE, MEANJIN. Enter Cool 3d World."
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
+      <GoogleAnalytics trackPageViews />
+      <Component {...pageProps} key={router.route} />
       <Analytics />
     </AnimatePresence>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
